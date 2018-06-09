df114fc59a86407f35a5bded77bed74cda5fb87e
DESKTOP-U0FF8H2\kiangmusa <kiangmusa23>
2018-06-09 20:58:06 +0800

    9 juni 2018 (2)

--- on1/main3.m
+++ on1/main3.m
@@ -2,7 +2,8 @@ clear;
 close all;
 
 asli= imread('is1.png');
-%  asli = asli1(50:150,50:150,:); 
+% asli = asli1(1:200,1:200,:); 
+% asli = hasil;
 baris = size(asli,1);
 kolom = size(asli,2);
 a = zeros(baris+2,kolom+2,3);
@@ -11,75 +12,90 @@ a(2:baris+1,2:kolom+1,2)=asli(:,:,2);
 a(2:baris+1,2:kolom+1,3)=asli(:,:,3);
 z = zeros(baris,kolom,3);
 g=0;
+s = 10;
 d=[];
-for i = 2 : size(a,1)-2
-    for j = 2 : size(a,2)-2
-        i
-        j
-        if(i-1==1) 
-            a(i-1,j-1:j+1,1)=a(i,j,1);
-            a(i-1,j-1:j+1,2)=a(i,j,2);
-            a(i-1,j-1:j+1,3)=a(i,j,3);
-        end
-        if(j-1==1)
-            a(i-1:i+1,j-1,1)=a(i,j,1);
-            a(i-1:i+1,j-1,2)=a(i,j,2);
-            a(i-1:i+1,j-1,3)=a(i,j,3);
-        end
-        if(i+1 == size(a,1))
-            a(i+1,j-1:j+1,1)=a(i,j,1);
-            a(i+1,j-1:j+1,2)=a(i,j,2);
-            a(i+1,j-1:j+1,3)=a(i,j,3);
-        end
-        if(j+1 == size(a,2))
-            a(i-1:i+1,j+1,1)=a(i,j,1);
-            a(i-1:i+1,j+1,2)=a(i,j,2);
-            a(i-1:i+1,j+1,3)=a(i,j,3);
-        end
-        b = a(i-1:i+1,j-1:j+1,:);
-        w1 = zeros(1,3);
-        w1(1,1) = a(i,j,1);
-        w1(1,2) = a(i,j,2);
-        w1(1,3) = a(i,j,3);
-       
-        for k=1:3
-            for l=1:3
-                c = 1;
-                w2 = zeros(1,3);
-%                 w2(1,1) = b(k,l,1);
-%                 w2(1,2) = b(k,l,2);
-%                 w2(1,3) = b(k,l,3);
-                
-                while(c>0)
-                    w2(1,1) = b(k,l,1);
-                    w2(1,2) = b(k,l,2);
-                    w2(1,3) = b(k,l,3);
-                    w1
-                    w2
-                    ok = jarak(w1,w2)
-                    if(ok<100 && ok>30)
-                        d=[d;
-                            i j k l];
-                        g=g+1;
-                        if(b(k,l,1)+5<=255)
-                            b(k,l,1)=b(k,l,1)+5
-                        else if (b(k,l,2)-5>=0)
-                            b(k,l,2)=b(k,l,2)-5
-                            else if(b(k,l,3)+5<=255)
-                                b(k,l,3)=b(k,l,3)+5
+per = 31;
+hit = 0;
+% while (per>30)
+    hit = hit+1;
+    per = 0;
+    for i = 2 : size(a,1)-2
+        for j = 2 : size(a,2)-2
+            
+            if(i-1==1) 
+                a(i-1,j-1:j+1,1)=a(i,j,1);
+                a(i-1,j-1:j+1,2)=a(i,j,2);
+                a(i-1,j-1:j+1,3)=a(i,j,3);
+            end
+            if(j-1==1)
+                a(i-1:i+1,j-1,1)=a(i,j,1);
+                a(i-1:i+1,j-1,2)=a(i,j,2);
+                a(i-1:i+1,j-1,3)=a(i,j,3);
+            end
+            if(i+1 == size(a,1))
+                a(i+1,j-1:j+1,1)=a(i,j,1);
+                a(i+1,j-1:j+1,2)=a(i,j,2);
+                a(i+1,j-1:j+1,3)=a(i,j,3);
+            end
+            if(j+1 == size(a,2))
+                a(i-1:i+1,j+1,1)=a(i,j,1);
+                a(i-1:i+1,j+1,2)=a(i,j,2);
+                a(i-1:i+1,j+1,3)=a(i,j,3);
+            end
+            b = a(i-1:i+1,j-1:j+1,:);
+            w1 = zeros(1,3);
+            w1(1,1) = a(i,j,1);
+            w1(1,2) = a(i,j,2);
+            w1(1,3) = a(i,j,3);
+
+            for k=1:3
+                for l=1:3
+                    
+                    c = 1;
+                    ulang =0;
+                    w2 = zeros(1,3);
+    %                 w2(1,1) = b(k,l,1);
+    %                 w2(1,2) = b(k,l,2);
+    %                 w2(1,3) = b(k,l,3);
+
+                    while(c>0)
+                        
+                        w2(1,1) = b(k,l,1);
+                        w2(1,2) = b(k,l,2);
+                        w2(1,3) = b(k,l,3);
+                        w1;
+                        w2;
+                        ok = jarak(w1,w2);
+                        if(ok<100 && ok>30)
+                            per = per+1;
+                            ulang = ulang+1;
+                            
+                            d=[d;
+                                i j k l];
+                            g=g+1;
+                            if ulang<10
+                                if(b(k,l,1)+s<=255)
+                                    b(k,l,1)=b(k,l,1)+s;
+                                else if (b(k,l,2)-s>=0)
+                                    b(k,l,2)=b(k,l,2)-s;
+                                    else if(b(k,l,3)-s>=0)
+                                        b(k,l,3)=b(k,l,3)-s;
+                                        end
+                                    end
                                 end
+                            else  b(k,l,3)=b(k,l,3)+s;
+                            c = 1;
                             end
+                        else 
+                            c = -1;
                         end
-                        c = 1;
-                    else 
-                        c = -1;
                     end
                 end
             end
+            a(i-1:i+1,j-1:j+1,:) = b;
         end
-        a(i-1:i+1,j-1:j+1,:) = b;
     end
-end
+% end
 hasil = uint8(a(2:baris+1,2:kolom+1,:));
 imshow(hasil);
         
--- on1/pemetaan.m
+++ on1/pemetaan.m
@@ -12,6 +12,7 @@ centroB = (centro);
 P_A = size(centroB,1);
 P_B = size(centroB,2);
 C = zeros(P_A,P_B);
+se = 10;
 centro2 = centroB;
 a =1;
 b=0;
@@ -27,15 +28,15 @@ while a ~=0
             hit = jarak(w1,w2);
             if(hit>30 && hit<100)
                 a = a+1;
-                if(centroB(1,j)+5<=255)
-                    C(1,j)=C(1,j)+5;
-                    centroB(1,j)=centroB(1,j)+5;
-                else if (centroB(2,j)-5>=0)
-                    C(2,j)=C(2,j)-5;
-                    centroB(2,j)=centroB(2,j)-5;
-                    else if(centroB(3,j)+5<=255)
-                        C(3,j)=C(3,j)+5;
-                        centroB(3,j)=centroB(3,j)+5;
+                if(centroB(1,j)+se<=255)
+                    C(1,j)=C(1,j)+se;
+                    centroB(1,j)=centroB(1,j)+se;
+                else if (centroB(2,j)-se>=0)
+                    C(2,j)=C(2,j)-se;
+                    centroB(2,j)=centroB(2,j)-se;
+                    else if(centroB(3,j)-se>=0)
+                        C(3,j)=C(3,j)-se;
+                        centroB(3,j)=centroB(3,j)-se;
                         end
                     end
                 end
